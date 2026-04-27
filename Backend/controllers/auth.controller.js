54a20e0db62faf16be31098a61415a73e71fe4de
EyobTesfaye8 <honore8a@gmail.com>
2026-04-27 10:02:09 +0300

    a little fix on register

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -1,21 +1,27 @@
 import { supabase } from '../config/supabaseClient.js'
+import joi, { string } from 'joi'
+import bcrypt from 'bcrypt'
 
+const userSchema = joi.object({
+    first_name: string().min(3).required(),
+    last_name:  string().min(3),
+    password:   string().min(8).required().max(20),
+    email:      string().pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/).required(),
+    username:   string().pattern(/^[a-zA-Z0-9_]{5,}$/).required()
+})
 export const register = async (req, res) => {
     try{
         const { first_name, last_name, email, password, username} = req.body
-        
-        const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
-        
-        if (!regex.test(email)) throw new Error('Your email is horseshitv( ፋንድያ )')
-        const { data, error } = await supabase.from("User").insert(first_name, last_name, email, password, username)
+        const { err, value } = userSchema.validate(req.body)
+        if (err) throw new Error("you bitch ass nigga, fix your shit")
 
-        if (error){
-            res.status(500).json({error})
-        }
+        const { data, error } = await supabase.from("User").insert(first_name, last_name, email, bcrypt(password), username)
 
+        if (error) throw new Error("somthing bad happened, in the db") 
         res.status(201).json(data)
     }catch(error){
-        
+        console.log(error)
+        res.status(500).json(error)
     }
 }
 export const login = async (req, res) => {
