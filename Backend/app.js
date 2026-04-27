406821b5c1556a2cfc5618643afd11356a47db3b
EyobTesfaye8 <honore8a@gmail.com>
2026-04-27 13:32:53 +0300

    login functionality done

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { register } from './controllers/auth.controller.js'
+import { login, register } from './controllers/auth.controller.js'
 const app = express()
 app.use(express.json())
 
 app.post('/users', register)
+app.post('/login', login)
 
 app.listen(3000, () => {
   console.log('Server running on port 3000');
--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -1,13 +1,21 @@
 import { supabase } from '../config/supabaseClient.js'
 import joi from 'joi'
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv';
 
+dotenv.config()
 const userSchema = joi.object({
     first_name: joi.string().min(3).required(),
     last_name:  joi.string().min(3),
     password:   joi.string().min(8).required().max(20),
     email:      joi.string().pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/).required(),
     username:   joi.string().pattern(/^[a-zA-Z0-9_]{5,}$/).required()
+})
+
+const loginSchema = joi.object({
+    username: joi.string().pattern(/^[a-zA-Z0-9_]{5,}$/).required(),
+    password: joi.string().min(8).required().max(20)
 })
 
 const hashPasword = async (password) =>{
@@ -39,6 +47,31 @@ export const register = async (req, res) => {
         res.status(500).json(error)
     }
 }
-// export const login = async (req, res) => {
+
+export const login = async (req, res) => {
+    try {
+        const { error: validationError, value } = loginSchema.validate(req.body)
+        const { username, password } = req.body
+        const { data, error:dberror } = await supabase 
+                                        .from('User')
+                                        .select('*')
+                                        .eq('username', username)
+        if (!data) return res.status(404).json({message: "You are not registered, please register first. (ደደብ ነህ እንዴ)"});
+
+        const passwd = data[0].password
+        const isMatch = await bcrypt.compare(req.body['password'], passwd)
+        if (isMatch) {
+            const token = jwt.sign({username}, process.env.SECRET_KEY, { expiresIn: "2w" })
+            res.status(200).json({'access_token':token})
+        }
+        else{
+            throw new Error('Invalid credentials')
+        }
+    } catch (error) {
+        res.status(500).json(error.message)
+    }
+}
+
+export const logout = async (req, res) => {
     
-// }//
+}
