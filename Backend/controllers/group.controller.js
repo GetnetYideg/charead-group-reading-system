64b23e0a78a7bdc9bb9eff3d91c72f8e37269394
Getnet Yideg <getnetyideg54@gmail.com>
2026-05-03 15:12:08 +0300

    added search group modules

--- Backend/controllers/group.controller.js
+++ Backend/controllers/group.controller.js
@@ -32,3 +32,65 @@ export const createGroup = async(req, res) =>{
         res.status(500).json(error.message)
     }
 }
+
+export const getUsersGroup = async (req, res) => {
+    try {
+        const owner_id = req.user.id
+        const {data, error: dberror } = await supabase
+            .from("Group")
+            .select("*")
+            .eq("owner_id", owner_id)
+
+        if (dberror){
+             throw new Error (dberror.message)
+        }
+        
+        res.status(200).json(data)
+    } catch (error) {
+        res.status(500).json (error.message)
+    }
+}
+
+export const searchGroups = async (req, res) => {
+    try {
+        const slug = req.params.slug
+        const {data, error: dberror} = await supabase
+            .from("Group")
+            .select("*")
+            .eq("slug", slug)
+        
+        if (dberror){
+            throw new Error(dberror.message)
+        }
+
+        res.status(200).json(data)
+    } catch (error) {
+        res.status(500).json(error.message)
+    }
+}
+
+export const searchGroupsById = async (req, res) => {
+    try {
+        const group_id = req.params.id
+        const { data, error: dberror } = await supabase
+            .from("Group")
+            .select("*")
+            .eq("id", group_id)
+        
+        if (dberror){
+            throw new Error( dberror.message )
+        }
+
+        res.status(200).json(data)
+    } catch (error) {
+        res.status(500).json(data)
+    }
+}
+
+export const joinGroup = async (req, res) =>{
+
+}
+
+export const deleteGroup = async (req, res) =>{
+
+}
