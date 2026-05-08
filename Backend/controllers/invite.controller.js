61325df959537a8cdd81fb968c0229e7dded10df
Getnet Yideg <getnetyideg54@gmail.com>
2026-05-08 13:29:41 +0300

    added invitation controllers

---
+++ Backend/controllers/invite.controller.js
@@ -0,0 +1,90 @@
+import { supabase } from "../config/supabaseClient.js";
+import { joinGroup } from "./group.controller.js";
+
+export const sendInvitation = async (req, res) => {
+    try {
+        const from_user_id = req.user_id;
+        const to_user_id = req.params.user_id
+        const group_id = req.params.group_id
+
+        const {data, error: dberror} = await supabase
+            .from("Invitation")
+            .insert([{
+                to_user_id,
+                from_user_id,
+                group_id
+            }]).select()
+        
+        if(dberror){
+            throw new Error("something happend in the database")
+        }
+
+        res.status(200).json(data[0])
+    } catch (error) {
+        res.status(500).json(error.message)
+    }
+}
+
+export const getInvitations = async (req, res) =>{
+    try {
+        const user_id = req.user_id
+
+        const {data, error: dberror} = await supabase
+            .from("Invitation")
+            .select("*")
+            .eq("to_user_id", user_id)
+        
+        if (dberror){
+            throw new Error("Something is happend on the database")
+        }
+
+        res.status(200).json(data)
+
+    } catch (error) {
+        res.status(500).json(error.message)
+    }
+}
+
+export const acceptInvitations = async (req, res) =>{
+    try {
+        const user_id = req.user_id
+        const group_id = req.params.group_id
+
+        await joinGroup(req, res);
+
+        const { data, error: dberror } = supabase
+            .from("Invitation")
+            .delete()
+            .eq("to_user_id", user_id)
+            .eq("group_id", group_id)
+        
+        if  (dberror){
+            throw new Error("Something is happend on the database")
+        }
+
+        res.status(200).json({message: "You joined the group"})
+    } catch (error) {
+        res.status(500).json(error.message)
+    }
+}
+
+export const declineInvitation = async (req, res) => {
+    try {
+        const user_id = req.user_id
+        const group_id = req.params.group_id
+
+        const {data, error: dberror} = await supabase 
+            .from("Invitation")
+            .delete()
+            .eq("to_user_id", user_id)
+            .eq("group_id", group_id)
+        
+         if  (dberror){
+            throw new Error("Something is happend on the database")
+        }
+
+        res.status(200).json({message: "You declined the invitation"})
+    } catch (error) {
+        res.status(500).json(error.message)
+    }
+}
